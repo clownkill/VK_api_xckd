c031d1889ff270d62d782d32aca5b5c86c9735dd
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-01-10 10:21:14 +0300

    Добавил функцию для взаимодействия с vk_api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from urllib.parse import urlsplit
+from pprint import pprint
 
 import requests
-
+from dotenv import load_dotenv
 
 def fetch_file_name(url):
     url_file_path = urlsplit(url).path
@@ -24,13 +25,28 @@ def download_comics(comics_url):
         file.write(response.content)
 
 
+def check_vk(client_id, access_token):
+    api_version = '5.131'
+    method = 'groups.get'
+    params = {
+        'user_id': 80317736,
+    }
+    url = f'https://api.vk.com/method/{method}?{params}&access_token={access_token}&v={api_version}'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    pprint(response.json())
+
 def main():
+    load_dotenv()
+    CLIENT_ID = os.getenv('vk_client_id')
+    ACCESS_TOKEN = os.getenv('vk_access_token')
     url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     comics = response.json()
     download_comics(comics['img'])
     print(comics['alt'])
+    check_vk(CLIENT_ID, ACCESS_TOKEN)
 
 
 if __name__ == '__main__':
